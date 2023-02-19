d7ed8e29f9a6e6a49cc276e5fc5bfdea761f21b0
Seung Hyeon Lim <kevinlsh17@khu.ac.kr>
2023-02-20 01:12:54 +0900

    Impl: Home fragment banner adapter

--- app/src/main/java/com/example/shoppi_clone/HomeFragment.kt
+++ app/src/main/java/com/example/shoppi_clone/HomeFragment.kt
@@ -8,8 +8,13 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.findFragment
 import androidx.navigation.fragment.findNavController
-import org.json.JSONObject
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayout.Tab
+import com.google.android.material.tabs.TabLayoutMediator
+import com.google.gson.Gson
 
 class HomeFragment : Fragment() {
     override fun onCreateView(
@@ -20,11 +25,15 @@ class HomeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
-    private lateinit var titleValue: Title
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val toolbarTitle = view.findViewById<TextView>(R.id.toolbar_home_title)
+        val toolbarIcon = view.findViewById<ImageView>(R.id.toolbar_home_icon)
+        val viewpager = view.findViewById<ViewPager2>(R.id.viewpager_home_banner)
+        val viewpagerIndicator = view.findViewById<TabLayout>(R.id.viewpager_home_banner_indicator)
+
+        /** Navigation 예시
         val button = view.findViewById<Button>(R.id.btn_enter_product_detail)
         button.setOnClickListener {
             /**
@@ -34,25 +43,36 @@ class HomeFragment : Fragment() {
              */
             findNavController().navigate(R.id.action_home_to_product_detail) //Navigation 사용
         }
+        */
 
         val assetLoader = AssetLoader()
-        val homeData = assetLoader.getJsonString(requireContext(), "home.json")
+        val homeJsonString = assetLoader.getJsonString(requireContext(), "home.json")
 
-        val toolbarTitle = view.findViewById<TextView>(R.id.toolbar_home_title)
-        val toolbarIcon = view.findViewById<ImageView>(R.id.toolbar_home_icon)
+        if (!homeJsonString.isNullOrEmpty()) {
+            val gson = Gson()
+            val homeData = gson.fromJson(homeJsonString, HomeData::class.java)
 
-        if (!homeData.isNullOrEmpty()) {
-            val jsonObject = JSONObject(homeData)
-            val title = jsonObject.getJSONObject("title")
-            val text = title.getString("text")
-            val iconUrl = title.getString("icon_url")
-
-            titleValue = Title(text, iconUrl)
-            toolbarTitle.text = titleValue.text
+            toolbarTitle.text = homeData.title.text
             GlideApp.with(this)
-                .load(titleValue.icon_url)
+                .load(homeData.title.iconUrl)
                 .into(toolbarIcon)
-        }
 
+            viewpager.adapter = HomeBannerAdapter().apply {
+                submitList(homeData.topBanners)
+            }
+
+            val pageWidth = resources.getDimension(R.dimen.viewpager_item_width)
+            val pageMargin = resources.getDimension(R.dimen.viewpager_item_margin)
+            val screenWidth = resources.displayMetrics.widthPixels
+            val offset = screenWidth - pageWidth - pageMargin
+
+            viewpager.offscreenPageLimit = 3
+            viewpager.setPageTransformer { page, position ->
+                page.translationX = position * -offset
+            }
+            TabLayoutMediator(viewpagerIndicator, viewpager) { tab, position ->
+
+            }.attach()
+        }
     }
 }
